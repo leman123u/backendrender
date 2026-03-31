593634f0b8341974357a1a32261aeb5598c08f71
leman123u <mturkan852@gmail.com>
2026-04-01 02:18:47 +0400

    Change UUID to String for reset token in UserService

--- demo/src/main/java/personalbudget/service/UserService.java
+++ demo/src/main/java/personalbudget/service/UserService.java
@@ -1,26 +1,28 @@
 package personalbudget.service;
 
 import java.util.Optional;
-import java.util.UUID;
+
 
 import personalbudget.entity.UserEntity;
 
 public interface UserService {
 	
 	
-	UserEntity findByEmail(String email);
+UserEntity findByEmail(String email);
 
     UserEntity save(UserEntity user);
 
     Optional<UserEntity> findByResetToken(String token);
 
 
-    UUID createResetToken(String email);
+    String createResetToken(String email);
 
     void resetPassword(String token, String newPassword);
 
     boolean existsByEmail(String email);
     
     void sendEmail(String to, String link);
+    
+    
 
 }
