d4b45cd5e3e504265840f21dfe3cebf50494fac5
leman123u <mturkan852@gmail.com>
2026-04-01 02:15:15 +0400

    Update UserRepository.java

--- demo/src/main/java/personalbudget/repository/UserRepository.java
+++ demo/src/main/java/personalbudget/repository/UserRepository.java
@@ -1,5 +1,4 @@
 package personalbudget.repository;
-
 import java.util.Optional;
 
 
@@ -7,17 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import personalbudget.entity.UserEntity;
 
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import personalbudget.entity.UserEntity;
-
 public interface UserRepository extends JpaRepository<UserEntity, Long>{
-	Optional<UserEntity> findByEmail(String email);
+	 Optional<UserEntity> findByEmail(String email);
 
-    Optional<UserEntity> findByResetToken(String  resetToken);
     
     boolean existsByEmail(String email);
+    
+    Optional<UserEntity> findByResetToken(String token);
 	
 
 }
