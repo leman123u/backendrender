4a44298bc54230095f41a48b11de251a3cd15b51
leman123u <mturkan852@gmail.com>
2026-04-01 01:27:49 +0400

    Add UUID and email dependencies to UserServiceImpl

--- demo/src/main/java/personalbudget/service/UserServiceImpl.java
+++ demo/src/main/java/personalbudget/service/UserServiceImpl.java
@@ -2,10 +2,12 @@ package personalbudget.service;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
-
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
