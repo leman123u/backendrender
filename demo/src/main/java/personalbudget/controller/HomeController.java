27647093bcc8b44c2e9036d1450e8ccd1bdb59c0
leman123u <mturkan852@gmail.com>
2026-04-05 01:34:58 +0400

    Update HomeController.java

--- demo/src/main/java/personalbudget/controller/HomeController.java
+++ demo/src/main/java/personalbudget/controller/HomeController.java
@@ -1,1 +1,16 @@
+package personalbudget.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	 @GetMapping("/")
+	    public String home() {
+	        return "Running ";
+	    }
+	
+	
+
+}
 
