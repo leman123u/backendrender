8a52b0b041dac919ed89d8f3e4cc70665857716d
leman123u <mturkan852@gmail.com>
2026-04-04 15:10:31 +0400

    Refactor UserRepository by adjusting method spacing

--- demo/src/main/java/personalbudget/repository/UserRepository.java
+++ demo/src/main/java/personalbudget/repository/UserRepository.java
@@ -1,4 +1,5 @@
 package personalbudget.repository;
+
 import java.util.Optional;
 
 
@@ -7,12 +8,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import personalbudget.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long>{
-	 Optional<UserEntity> findByEmail(String email);
+	
+	
 
+    Optional<UserEntity> findByEmail(String email);
+
+   // Optional<UserEntity> findByResetToken(String  resetToken);
     
     boolean existsByEmail(String email);
     
     Optional<UserEntity> findByResetToken(String token);
-	
-
+    
 }
