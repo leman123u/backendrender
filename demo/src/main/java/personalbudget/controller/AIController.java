a22b1722b3823c61c69e5b13a920445cc4824a40
leman123u <mturkan852@gmail.com>
2026-04-05 01:04:38 +0400

    Update AIController.java

--- demo/src/main/java/personalbudget/controller/AIController.java
+++ demo/src/main/java/personalbudget/controller/AIController.java
@@ -1,1 +1,31 @@
+package personalbudget.controller;
+
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import personalbudget.service.AIService;
+
+@RestController
+@RequestMapping("/api/ai")
+@CrossOrigin(origins = "*")
+public class AIController {
+	
+	  @Autowired
+	    private AIService aiService;
+	    // ✅ AI CHAT ENDPOINT
+	  @PostMapping("/ask")
+	  public String askAI(@RequestBody Map<String, String> body) {
+	      return aiService.askAI(body.get("prompt"));
+	  }
+	
+	
+
+}
 
