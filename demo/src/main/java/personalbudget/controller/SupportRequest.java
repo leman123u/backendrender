6117a1db8523e068d5220d62770f98daf6c4513b
leman123u <mturkan852@gmail.com>
2026-04-05 01:01:59 +0400

    Add SupportRequest class with email and message fields

--- demo/src/main/java/personalbudget/controller/SupportRequest.java
+++ demo/src/main/java/personalbudget/controller/SupportRequest.java
@@ -1,1 +1,14 @@
+package personalbudget.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter 
+@Setter
+public class SupportRequest {
+	
+	private String email;
+    private String message;
+
+}
 
